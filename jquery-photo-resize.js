9fe4f38b65c39f59a02c3bd34f420cd39ce7af2e
Mark <github880520@mstier.de>
2012-04-02 21:26:33 +0200

    added shrinkToWindow function

--- jquery-photo-resize.js
+++ jquery-photo-resize.js
@@ -39,3 +39,61 @@
 	};
 
 }(jQuery));
+
+
+/*
+* Adjust some element's right and lower boundaries on browser window resize
+* 
+* @example: $('selector').shrinkToWindow();
+*/
+
+(function ($) {
+    $.fn.shrinkToWindow = function (options) {
+
+        var element = $(this),
+        defaults = {
+            bottomSpacing: 15,
+            rightSpacing: 20,
+            unscaledHeight: -1,
+            unscaledWidth: -1,
+        };
+
+        $(element).load(function () {
+            init();
+            changeDimensions();
+
+            $(window).bind('resize', function () {
+                changeDimensions();
+            });
+        });
+
+        options = $.extend(defaults, options);
+
+        function init() {
+            // remember initial picture size (used as maximum size)
+            options.unscaledHeight = $(element).height();
+            options.unscaledWidth = $(element).width();
+        }
+        function changeDimensions() {
+            var maxDisplayHeight = $(window).height() - $(element).offset().top - options.bottomSpacing;
+            var maxDisplayWidth  = $(window).width() - $(element).offset().left - options.rightSpacing;
+            var desiredHeight = maxDisplayHeight < options.unscaledHeight ? maxDisplayHeight : options.unscaledHeight;
+            var desiredWidth  = maxDisplayWidth  < options.unscaledWidth  ? maxDisplayWidth  : options.unscaledWidth;
+            var currHeight = $(element).height();
+            var currWidth  = $(element).width();
+
+            if ( currHeight != desiredHeight || currWidth != desiredWidth ) {
+                // keep aspect ratio
+                if ( desiredHeight / options.unscaledHeight <= desiredWidth / options.unscaledWidth ) {
+                    $(element).height(desiredHeight);
+                    $(element).width(options.unscaledWidth * desiredHeight / options.unscaledHeight);
+                } else {
+                    $(element).height(options.unscaledHeight * desiredWidth / options.unscaledWidth);
+                    $(element).width(desiredWidth);
+                }
+            }
+        }
+    };
+
+}(jQuery));
+
